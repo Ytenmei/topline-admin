55c89bde81f52b669543624c1164f020d2b64e54
Ytenmei <ytm980582306@163.com>
2019-06-22 23:15:14 +0800

    导入图片资源

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-      <p>APP Component</p>
+    <p>APP component</p>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
   </div>
 </template>
 
 <script>
-export default {
 
-}
 </script>
 
 <style lang="less" scoped>
