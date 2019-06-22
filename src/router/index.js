8596d12467bd64796d9f121d75a8c3eb237c4ec5
Ytenmei <ytm980582306@163.com>
2019-06-22 23:57:03 +0800

    配置路由导航

--- src/router/index.js
+++ src/router/index.js
@@ -5,5 +5,16 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: () => import('@/views/home')
+      // @ 表示SRC目录，无论当前当前文件在哪里， @都是src
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: () => import('@/views/login')
+    }
   ]
 })
