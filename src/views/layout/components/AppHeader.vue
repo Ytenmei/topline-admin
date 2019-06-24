ad2e195955a5cd2dfbb02cbae56851ac08c85e66
Ytenmei <ytm980582306@163.com>
2019-06-24 21:33:39 +0800

    layout初始化并配置路由

---
+++ src/views/layout/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>AppHeader components</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <AppHeader/>
+    <AppAside/>
+    <!-- Home 取，发布文章、评论管理.... -->
+  </div>
+</template>
+
+<script>
+import AppAside from './components/AppAside'
+import AppHeader from './components/AppHeader'
+
+export default {
+  name: 'AppLayout',
+  components: {
+    AppAside,
+    AppHeader
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
