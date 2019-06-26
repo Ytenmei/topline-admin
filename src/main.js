cb0be78708a802f3ba64b84cd4f38af1bc0d7920
Ytenmei <ytm980582306@163.com>
2019-06-26 21:34:05 +0800

    使用axios请求拦截器同意授权

--- src/main.js
+++ src/main.js
@@ -10,6 +10,29 @@ import 'nprogress/nprogress.css'
 
 // 配置 axios 的基础路由
 axios.defaults.baseURL = 'http://toutiao.course.itcast.cn/mp/v1_0/'
+
+// Axios 请求拦截器
+// 所有axios发起的请求都需要经过这里
+// return config 允许通过的方式
+// config 是本次请求相关的配置
+axios.interceptors.request.use((config) => {
+  // console.log(config)
+  // 添加本地储存用户信息
+  const userInfo = JSON.parse(window.localStorage.getItem('user_info'))
+  // 让所有经过这里的aixos请求都添加token信息
+  config.headers.Authorization = `Bearer ${userInfo.token}`
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+// Axios 响应拦截器
+// 所有axios发情的响应都需要经过这里
+axios.interceptors.response.use((response) => {
+  return response
+}, error => {
+  return Promise.reject(error)
+})
 //  往原型对象中添加成员
 Vue.prototype.$http = axios
 Vue.use(ElementUI)
