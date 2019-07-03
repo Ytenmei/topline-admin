242b10615f144b56f9b7cf679fdc77dc97b03a8a
Ytenmei <ytm980582306@163.com>
2019-07-03 11:40:37 +0800

    封面处理-上传确定完成

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -15,7 +15,22 @@
       :visible.sync="dialogVisible"
       width="30%"
       >
-      <span>这是一段信息</span>
+      <el-tabs value="first">
+        <el-tab-pane label="上传图片" name="first">
+          <el-upload
+          class="avatar-uploader"
+          action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+          :headers="{ Authorization: `Bearer ${$store.state.user.token}`}"
+          name="image"
+          :on-success="handleUpload"
+          :show-file-list="false">
+          <!-- 上传成功，要预览的图片 -->
+          <img v-if="imageUrl" :src="imageUrl" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+        </el-tab-pane>
+        <el-tab-pane label="媒体库" name="second">媒体库</el-tab-pane>
+      </el-tabs>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
@@ -29,17 +44,47 @@ export default {
   name: 'UploadImage',
   data () {
     return {
-      dialogVisible: false
+      dialogVisible: false,
+      imageUrl: null
     }
   },
   methods: {
     handleShowMdiaBox () {
       // 显示弹窗
       this.dialogVisible = true
+    },
+    handleUpload (res) {
+      this.imageUrl = res.data.url
     }
   }
 }
 </script>
+
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
 
 <style lang="less" scoped>
 .box-wrap {
