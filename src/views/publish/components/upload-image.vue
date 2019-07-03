be5a5e0912264e59de7377941c644c531d7a75b1
Ytenmei <ytm980582306@163.com>
2019-07-03 11:14:28 +0800

    封面处理-显示对话框

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="box-wrap">
-    <p>点击选择图片</p>
-    <i
-    style="margin-top: 20px;
-    font-size: 90px;color: #eee;"
-    class="iconfont icon-image_upload"
-    ></i>
-    <img src="" width="120">
+  <div>
+    <div class="box-wrap" @click="handleShowMdiaBox">
+      <p>点击选择图片</p>
+      <i
+      style="margin-top: 20px;
+      font-size: 90px;color: #eee;"
+      class="iconfont icon-image_upload"
+      ></i>
+      <img src="" width="120">
+    </div>
+      <!-- 对话框 -->
+    <el-dialog
+      title="素材"
+      :visible.sync="dialogVisible"
+      width="30%"
+      >
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -14,7 +28,15 @@
 export default {
   name: 'UploadImage',
   data () {
-    return {}
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleShowMdiaBox () {
+      // 显示弹窗
+      this.dialogVisible = true
+    }
   }
 }
 </script>
